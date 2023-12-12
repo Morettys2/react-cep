8a66843c542f1ccf2f50459b6f68e471ba4f7019
Morettys2 <m0rettyy20@gmail.com>
2023-12-11 21:18:21 -0300

    H1 cep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ export default function Home() {
   return (
     <div className='flex flex-col p-4'>
       <div className='flex flex-row space-x-1 mb-4'>
-        <h1 className='text-blue-800 '>Hello</h1>
-        <h1 className='text-blue-800 '>World</h1>
+        <h1 className='text-blue-800 '>Cep</h1> 
       </div>
-
-      
     </div>
   )
 }
