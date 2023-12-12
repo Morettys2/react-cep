a3f229216968004d917d4106073a1561d1fd616f
Morettys2 <m0rettyy20@gmail.com>
2023-12-11 21:12:26 -0300

    Meu primeirio commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col p-4'>
+      <div className='flex flex-row space-x-1 mb-4'>
+        <h1 className='text-blue-800 '>Hello</h1>
+        <h1 className='text-blue-800 '>World</h1>
+      </div>
+
+      <div>
+        <h1>Meguiinha</h1>
+      </div>
+    </div>
+  )
+}
